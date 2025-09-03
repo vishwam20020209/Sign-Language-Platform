d05b9e83b66f15e03945e91f170c8a7ad6c4080d
vishwam20020209 <167871263+vishwam20020209@users.noreply.github.com>
2025-09-03 21:47:32 +0530

    assets

---
+++ assets/index.js
@@ -0,0 +1,139 @@
+import brainwave from "./Divine DevOps.png";
+import check from "./check.svg";
+import brainwaveSymbol from "./Divine DevOps-symbol.png";
+import brainwaveWhiteSymbol from "./Divine DevOps-symbol.png";
+import play from "./play.svg";
+import gradient from "./gradient.png";
+import smallSphere from "./4-small.png";
+import grid from "./grid.png";
+import check2 from "./check-02.svg";
+import loading1 from "./loading-01.svg";
+import homeSmile from "./home-smile.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import plusSquare from "./plus-square.svg";
+import recording03 from "./recording-03.svg";
+import recording01 from "./recording-01.svg";
+import disc02 from "./disc-02.svg";
+import chromecast from "./chrome-cast.svg";
+import sliders04 from "./sliders-04.svg";
+import loading from "./loading.png";
+import background from "./background.png";
+
+import curve from "./hero/curve.png";
+import robot from "./hero/robot.jpg";
+import heroBackground from "./hero/hero-background.jpg";
+
+import curve1 from "./collaboration/curve-1.svg";
+import curve2 from "./collaboration/curve-2.svg";
+import discord from "./collaboration/discord.png";
+import figma from "./collaboration/figma.png";
+import framer from "./collaboration/framer.png";
+import notion from "./collaboration/notion.png";
+import photoshop from "./collaboration/photoshop.png";
+import protopie from "./collaboration/protopie.png";
+import raindrop from "./collaboration/raindrop.png";
+import slack from "./collaboration/slack.png";
+
+import service1 from "./services/service-1.png";
+import service2 from "./services/service-2.png";
+import service3 from "./services/service-3.png";
+
+import lines from "./pricing/lines.svg";
+import stars from "./pricing/stars.svg";
+
+import coins from "./roadmap/coins.png";
+import done from "./roadmap/done.svg";
+import hero from "./roadmap/hero.png";
+import roadmap1 from "./roadmap/image-1.png";
+import roadmap2 from "./roadmap/image-2.png";
+import roadmap3 from "./roadmap/image-3.png";
+import roadmap4 from "./roadmap/image-4.png";
+import undone from "./roadmap/undone.svg";
+
+import benefitCard1 from "./benefits/card-1.svg";
+import benefitCard2 from "./benefits/card-2.svg";
+import benefitCard3 from "./benefits/card-3.svg";
+import benefitCard4 from "./benefits/card-4.svg";
+import benefitCard5 from "./benefits/card-5.svg";
+import benefitCard6 from "./benefits/card-6.svg";
+import benefitImage2 from "./benefits/image-2.png";
+import benefitImage3 from "./benefits/image-3.png";
+import benefitImage4 from "./benefits/image-4.png";
+import benefitImage5 from "./benefits/image-5.png";
+import benefitImage6 from "./benefits/image-6.png";
+import benefitImage7 from "./benefits/image-7.png";
+import benefitImage8 from "./benefits/image-8.png";
+import discordBlack from "./socials/discord.svg";
+import facebook from "./socials/facebook.svg";
+import instagram from "./socials/instagram.svg";
+import telegram from "./socials/telegram.svg";
+import twitter from "./socials/twitter.svg";
+
+export {
+  brainwave,
+  check,
+  check2,
+  loading1,
+  brainwaveSymbol,
+  brainwaveWhiteSymbol,
+  play,
+  gradient,
+  smallSphere,
+  grid,
+  homeSmile,
+  file02,
+  searchMd,
+  plusSquare,
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+  loading,
+  background,
+  curve,
+  robot,
+  heroBackground,
+  curve1,
+  curve2,
+  discord,
+  figma,
+  framer,
+  notion,
+  photoshop,
+  protopie,
+  raindrop,
+  slack,
+  service1,
+  service2,
+  service3,
+  lines,
+  stars,
+  coins,
+  done,
+  hero,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  undone,
+  benefitCard1,
+  benefitCard2,
+  benefitCard3,
+  benefitCard4,
+  benefitCard5,
+  benefitCard6,
+  benefitImage2,
+  benefitImage3,
+  benefitImage4,
+  benefitImage5,
+  benefitImage6,
+  benefitImage7,
+  benefitImage8,
+  discordBlack,
+  facebook,
+  instagram,
+  telegram,
+  twitter,
+};
